4401bcaa627629ec5fbb124d3acd215ab6241a88
leanferreira17cba <127784553+leanferreira17cba@users.noreply.github.com>
2024-08-22 09:57:10 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         Persona marta = new Persona();
 
-        System.out.println(marta.edad);
+        /*System.out.println(marta.edad);
         System.out.println(marta.anioDeNacimiento);
         System.out.println(marta.dni);
         System.out.println(marta.coordenadaX);
@@ -14,6 +14,18 @@ public class Main {
         System.out.println(marta.altura);
         System.out.println(marta.patrimonioEnPesos);
         System.out.println(marta.sexo);
-        System.out.println(marta.estaVivo);
+        System.out.println(marta.estaVivo);*/
+
+        byte nuevaEdad = 12;
+
+        Persona ramon = new Persona();
+        ramon.setEdad(nuevaEdad);
+
+        System.out.println(ramon.getEdad());
+
+        Persona ana = new Persona((byte) 46);
+
+        System.out.println(ana.getEdad());
+
     }
 }
